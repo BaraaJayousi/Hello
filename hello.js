2c149c5f0fd594267545bd3d53eb50612d3c0167
Baraa <b.jayousi98@gmail.com>
2020-08-20 11:16:05 +0300

    Added a comment

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
+// this function add two numbers
 function sum (num1, num2)
 {
   return num1 + num2;
 }
+// this function subtracts two numbers
 function subtract (num1, num2) 
 {
 	return num1 - num2
